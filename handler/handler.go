50e18b1ae960c3404558da6efa589f3a8ef0d90e
shubham <shubham.asati2646@gmail.com>
2020-01-13 23:23:22 +0530

    removed path deliminater dependency

--- handler/handler.go
+++ handler/handler.go
@@ -118,7 +118,7 @@ func generateOutput(request *GenerateTemplateRequest) (*GenerateTemplateResponse
 		return nil, fmt.Errorf("requested apptype does not exists")
 	}
 	request.Library = strings.ReplaceAll(request.Library,"/",string(os.PathSeparator))
-	fmt.Println(request.Library)
+	
 	if !utils.LibExists(filepath.Join(request.AppType, request.Library)) {
 		return nil, fmt.Errorf("request library does not exists")
 	}
